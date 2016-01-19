8791129aa9b76d269faef9a4a8288d39af93b23b
David Chenaux <david.chenaux@unifr.ch>
2016-01-19 14:00:00 +0100

    Removing the reports which are not compatible with python3 and updating the requirements list

--- reports/utils.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('reports', 'templates/default'),
-                  extensions=['jinja-ext.jinja2htmlcompress.HTMLCompress'])
-
-
-def render_report(template_name, context):
-    t = env.get_template(template_name)
-    return t.render(context)
-
