89c1a8699e5b76d640289a7ba862dcfe12a35d67
David Chenaux <david.chenaux@unifr.ch>
2016-01-20 16:37:20 +0100

    Gui improvements including : View file page Deleting files

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,6 @@
 from mongoengine import *
 from docdef import *
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask.ext.mongoengine import MongoEngine
 from flask_bootstrap import Bootstrap
 
@@ -11,11 +11,16 @@ Bootstrap(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html', files=File.objects)
+    return render_template('index.html', files=File.objects.order_by('-timestamp'))
 
-@app.route("/view_file")
-def view_file():
-    return render_template('view_file.html', files=File.objects)
+@app.route("/view_file/<file_id>")
+def view_file(file_id):
+    return render_template('view_file.html', files=File.objects(id=file_id))
+
+@app.route("/remove_file/<file_id>")
+def remove_file(file_id):
+    File.objects(id=file_id).delete()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
