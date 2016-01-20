75b550dd120c0366aad67843f1d2c65286b30c53
David Chenaux <david.chenaux@unifr.ch>
2016-01-20 15:13:38 +0100

    Adding the Flask Framework with bootstrap and mongoengine

--- gui/main.py
+++ gui/main.py
@@ -1,11 +1,21 @@
 from mongoengine import *
 from docdef import *
+from flask import Flask, render_template
+from flask.ext.mongoengine import MongoEngine
+from flask_bootstrap import Bootstrap
 
-connect('yoda')
+app = Flask(__name__)
+app.config['MONGODB_DB'] = 'yoda'
+db = MongoEngine(app)
+Bootstrap(app)
 
-for file in File.objects:
-    print(file.revision)
-    print(file.filename)
-    print(file.timestamp)
-    for line in file.lines:
-        print(line.lineno, line.data)
+@app.route("/")
+def index():
+    return render_template('index.html', files=File.objects)
+
+@app.route("/view_file")
+def view_file():
+    return render_template('view_file.html', files=File.objects)
+
+if __name__ == "__main__":
+    app.run(debug=True)
