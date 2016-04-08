6cb42a815905f88c21599434088cbaeea97d2154
David Chenaux <david.chenaux@unifr.ch>
2016-04-08 14:22:07 +0200

    Correction for lists with deepcopy in order to avoid inherited modifications

--- yoda/analyser.py
+++ yoda/analyser.py
@@ -32,6 +32,7 @@ from datetime import datetime
 import subprocess
 import sys
 import timeit
+import copy
 
 from mongoengine import *
 
@@ -42,7 +43,7 @@ from yoda.docdef import *
 class Yoda(bdb.Bdb):
     run = 0
     json_results = None
-    instrumented_types = (int, float, str)
+    instrumented_types = (int, float, str, list)
     #instrumented_types = (dict, bytes, bool, float, int, list, object, str, tuple)
     prev_lineno = defaultdict(int)
     prev_lineno['<module>'] = 0
@@ -58,13 +59,16 @@ class Yoda(bdb.Bdb):
         self.json_results = defaultdict(lambda: defaultdict(defaultdict))
 
     def _filter_locals(self, local_vars):
+        print("FILTER LOCALS")
         new_locals = {}
         for name, value in list(local_vars.items()):
             if name.startswith('__'):
                 continue
             if not isinstance(value, self.instrumented_types):
                 continue
-            new_locals[name] = [value]
+            new_locals[name] = [copy.deepcopy(value)]
+
+            print(new_locals)
 
         return new_locals
 
@@ -75,44 +79,49 @@ class Yoda(bdb.Bdb):
         return subprocess.check_output(['git', 'config', 'user.name'])
 
     def user_call(self, frame, args):
+        print("CALL EVENT")
         self.cur_framename = str(frame.f_code.co_name)
         self.prev_lineno[self.cur_framename] = frame.f_lineno
 
         self.set_step() # continue
 
     def user_line(self, frame):
+        print("---------------------------------------------------------------------------------------------------------------------------------------------------------")
+        print("LINE EVENT")
+        print("Premier", self.json_results)
         cur_lineno = frame.f_lineno
         lineno = self.prev_lineno[self.cur_framename]
 
         locals = self._filter_locals(frame.f_locals)
         filename = frame.f_globals['__file__']
 
-        print("LINE",self.prev_lineno,", FIRST LINE", frame.f_code.co_firstlineno)
-
         if locals:
             if not self.json_results[filename][self.cur_framename].get(lineno):
+                #print(locals)
+                #print(lineno)
                 self.json_results[filename][self.cur_framename][lineno] = locals
+                print("Deuxième",self.json_results)
+                print("stored")
             else:
+                print("pasla")
                 for k,v in locals.items():
                     if not self.json_results[filename][self.cur_framename][lineno].get(k):
                         self.json_results[filename][self.cur_framename][lineno][k] = v
                     else:
                         self.json_results[filename][self.cur_framename][lineno][k].append(v[0])
 
-
-            print(self.cur_framename,
-                type(self.json_results[filename][self.cur_framename][lineno]),
-                self.json_results[filename][self.cur_framename][lineno])
-
         self.prev_lineno[self.cur_framename] = cur_lineno
 
         self.set_step()
+        print("Troisième",self.json_results)
 
     def user_return(self, frame, value):
+        print("RETURN EVENT")
         self.cur_framename = '<module>'
         self.set_step()  # continue
 
     def user_exception(self, frame, exception):
+        print("EXCEPTION EVENT")
         name = frame.f_code.co_name or "<unknown>"
         print("exception in", name, exception)
         self.set_continue()  # continue
@@ -124,9 +133,10 @@ class Yoda(bdb.Bdb):
         sys.settrace(None)
 
         if self.json_results:
+            print(self.json_results)
 
             if settings.DEBUG:
-                pass
+                print(self.json_results)
             else:
                 for module_file, frames in self.json_results.items():
                     if 'analyser.py' not in module_file:
